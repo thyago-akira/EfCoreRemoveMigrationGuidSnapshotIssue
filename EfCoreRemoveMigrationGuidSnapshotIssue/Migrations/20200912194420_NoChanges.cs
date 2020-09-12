89b648f60fef281acbc0301ad383a3575774dbc1
Thyago Akira <thyakira@gmail.com>
2020-09-12 14:44:25 -0500

    Add migration without changes

---
+++ EfCoreRemoveMigrationGuidSnapshotIssue/Migrations/20200912194420_NoChanges.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EfCoreRemoveMigrationGuidSnapshotIssue.Migrations
+{
+    public partial class NoChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
